1f8689141ec4c965b3e1856a9daa8b425d75b427
Daniel Caldera <dan@wait3.com>
2022-10-03 19:44:04 -0500

    UPDATE: basic tensorflow conf

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+train_images, test_images = train_images / 255, test_images / 255
+
+class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+for i in range(10):
+    plt.subplot(2, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i][0]])
+
+plt.show()
